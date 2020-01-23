71aff87c4bc25cc881b21f3f7ff375f945ae36d4
Wajdijazz <mohamedwajdijaziri@gmail.com>
2020-01-23 10:05:02 +0100

    first commit

---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+
+declare const $: any;
+declare interface RouteInfo {
+    path: string;
+    title: string;
+    icon: string;
+    class: string;
+}
+export const ROUTES: RouteInfo[] = [
+    { path: '/dashboard', title: 'Dashboard',  icon: 'assessment', class: '' },
+    { path: '/user-profile', title: ' Persons',  icon:'account_box', class: '' },
+    { path: '/table-list', title: 'Clients',  icon:'sentiment_satisfied', class: '' },
+    { path: '/typography', title: 'Projects',  icon:'business_center', class: '' },
+    { path: '/icons', title: 'Daily Rates',  icon:'monetization_on', class: '' },
+    { path: '/icons', title: 'interventions',  icon:'touch_app', class: '' },
+
+
+
+
+];
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent implements OnInit {
+  menuItems: any[];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.menuItems = ROUTES.filter(menuItem => menuItem);
+  }
+  isMobileMenu() {
+      if ($(window).width() > 991) {
+          return false;
+      }
+      return true;
+  };
+}
