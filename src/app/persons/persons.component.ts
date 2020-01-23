ee024a500301337c9e0d652642b27f555385e526
MOHAMED WAJDI Jaziri <mohamedwajdijaziri@gmail.com>
2020-01-23 13:05:03 +0100

    create and list

--- src/app/persons/persons.component.ts
+++ src/app/persons/persons.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Person} from './person';
 import {Observable} from 'rxjs';
 import {PersonService} from '../services/person.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-persons',
@@ -10,16 +11,32 @@ import {PersonService} from '../services/person.service';
 })
 export class PersonsComponent implements OnInit {
 persons:Person[];
+person:Person={
 
-  constructor(private personService:PersonService) { }
+  personId:null,
+  firstName:'',
+  lastName:''
+}
+  constructor(private personService:PersonService,private router: Router) { }
 
   ngOnInit() {
-    this.personService.getPersons().subscribe((data:Person[])=>{
-this.persons=data;
-console.log((this.persons))
-    })
+  this.getAllPersons()
   }
-addPerson(){
+
+  getAllPersons(){
+
+    this.personService.getPersons().subscribe((data:Person[])=>{
+      this.persons=data;
+      console.log((this.persons))
+    })
+
+
+  }
+addPerson(data:Person){
+  this.personService.savePerson(data)
+  this.router.navigateByUrl(`persons`);
+  this.getAllPersons()
+
 
 
 }
