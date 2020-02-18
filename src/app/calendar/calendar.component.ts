1663c3aac47c8056032f2f17d6206e4286a11812
Jonathan <jonathan.chauvin@davidson.fr>
2020-02-18 17:58:41 +0100

    Calendar style NOK but functionnaly usable

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Subject} from 'rxjs';
-import {CalendarView} from 'angular-calendar';
+import {CalendarMonthViewDay, CalendarView} from 'angular-calendar';
 
 @Component({
   selector: 'app-calendar',
@@ -14,9 +14,47 @@ export class CalendarComponent implements OnInit {
     view: CalendarView = CalendarView.Month;
 
 
+    listDate: number[] = [];
+
     constructor() { }
 
     ngOnInit() {
+    }
+
+    beforeMonthViewRender({body}: { body: CalendarMonthViewDay[] }): void {
+        body.forEach(day => {
+            const dayOfMonth = day.date.getDate();
+            const month = day.date.getMonth();
+
+            if (day.isWeekend) {
+                day.cssClass = 'week-end';
+            }
+
+            const dayOff: boolean = this.listDate.filter((tmp) => tmp === dayOfMonth)[0] != null;
+
+            if (dayOff) {
+                day.cssClass = 'week-end';
+            }
+            if (!day.inMonth) {
+                day.cssClass = 'disabled';
+            }
+            // this.TAmap.forEach(TAlist => {
+            //     TAlist.forEach( TA => {
+            //         TA.timeranges.forEach(timerange => {
+            //             const date = new Date(timerange.date);
+            //             if (date.getUTCDate() === dayOfMonth && date.getMonth() === month && day.inMonth) {
+            //                 day.events.push({
+            //                     id: null,
+            //                     title: null,
+            //                     start: setHours(setMinutes(new Date(), 0), (timerange.daytime === 'AM') ? 3 : 18),
+            //                     color: {primary: this.setColorOfMonthCase(TA.dayObject), secondary: 'blue'},
+            //                     meta: [{item: TA.dayObject, role: TA.role}]
+            //                 });
+            //             }
+            //         });
+            //     });
+            // });
+        });
     }
 
 }
